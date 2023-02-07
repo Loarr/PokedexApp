9f6974c4a7be2e9b9626ed54c900c196b7a70413
Loarr <mark.surtees@hotmail.co.uk>
2023-02-07 19:01:33 +0000

    added pokedex info

--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="flex flex-col space-y-6">
         <h1 class="text-6xl text-gray-900 font-semibold">{{ dispName }}</h1>
+        <h2 class="text-4xl text-gray-900 font-semibold">{{ genus }}</h2>
+        <div class="flex items-center space-x-4">
+            <span>Weight</span><span>{{ pokemonDetails?.weight }}kg</span>
+            <span>Height</span><span>{{ pokemonDetails?.height }}m</span>
+        </div>
         <img :src="image" :alt="pokemonDetails?.name" class="w-64 h-64 rounded-lg" />
-        <div class="space-y-4">
-            <div class="flex items-center space-x-4">
-                <span>Primary Type:</span><TypeBar :pkmType="type1"/>
+        <div class="flex space-x-4 items-center">
+            <div class="">
+                <TypeBar :pkmType="type1"/>
             </div>
-            <div class="flex items-center space-x-4" v-if="type2">
-                <span>Secondary Type:</span><TypeBar :pkmType="type2"/>
+            <div class="" v-if="type2">
+                <TypeBar :pkmType="type2"/>
             </div>
         </div>
         <div>
-            <div class="flex items-center space-x-4">
-                <span>Weight</span><span>{{ pokemonDetails?.weight }}kg</span>
-            </div>
-            <div class="flex items-center space-x-4">
-                <span>Height</span><span>{{ pokemonDetails?.height }}m</span>
-            </div>
+            <span>{{ pokedex.flavor_text }}</span>
         </div>
     </div>
 </template>
@@ -38,6 +38,8 @@ const type1 = ref("")
 const type2 = ref("")
 
 const pokemonDetails = ref({})
+const pokedex = ref({})
+const genus = ref("")
 const image = ref("")
 
 // passing name as route parameter. Check router/index.js for more details
@@ -47,8 +49,6 @@ onMounted(()=>{
     
     pkmName.value = route.params.name.toLowerCase();
 
-    // pokemonDetails.value = pokemons.filter((item) => item.name.toLowerCase() === route.params.name.toLowerCase())[0];
-
     axios.get(`https://pokeapi.co/api/v2/pokemon/${pkmName.value}`).then(data =>{
         image.value = data.data.sprites.other['official-artwork'].front_default
         pokemonDetails.value = data.data
@@ -57,17 +57,14 @@ onMounted(()=>{
             type2.value = (data.data.types[1].type.name)
         }
         dispName.value = data.data.name[0].toUpperCase() + data.data.name.slice(1)
+    });
+
+    axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pkmName.value}`).then(data =>{
+        pokedex.value = data.data.flavor_text_entries[7]
+        genus.value = data.data.genera[7].genus
     })
 
 });
-
-// function capitalized(name) {
-//     const capitalizedFirst = name[0].toUpperCase();
-//     const rest = name.slice(1);
-
-//     return capitalizedFirst + rest;
-// },
-
 
 
 </script>
