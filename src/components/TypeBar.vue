b5612f7ac62ea80279e4300a1c6b30cec8cb2bc3
Loarr <mark.surtees@hotmail.co.uk>
2023-03-03 13:15:24 +0000

    created typebar component

---
+++ src/components/TypeBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex bg-[#9FA19F] text-white w-28 font-semibold">
+        <img :src="'/types/'+props.pkmType+'.png'" class="w-8"/><p class="text-center m-auto">{{ type }}</p>
+    </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    pkmType:{
+        type: String
+    }
+})
+
+const type = computed(()=>{
+    return props.pkmType[0]?.toUpperCase() + props.pkmType.slice(1)
+})
+
+
+</script>
+
